f4081095ef2dd78e6dcfcbff2ff2f6f0d81c5cfc
Dmitry <dimkkononov@gmail.com>
2025-12-26 15:22:59 +0300

    add logging in usecase

--- internal/usecase/task.go
+++ internal/usecase/task.go
@@ -7,6 +7,7 @@ import (
 	"sort"
 
 	"github.com/solumD/tasks-service/internal/model"
+	"github.com/solumD/tasks-service/pkg/logger"
 )
 
 var (
@@ -27,21 +28,33 @@ func NewTaskUsecase(taskRepo TaskRepo, log *slog.Logger) *taskUsecase {
 }
 
 func (u *taskUsecase) CreateTask(ctx context.Context, task *model.Task) (int, error) {
+	const fn = "taskUsecase.CreateTask"
+	log := u.log.With(logger.String("fn", fn))
+
 	if len(task.Title) == 0 {
 		return 0, ErrEmptyTitle
 	}
 
 	id, err := u.taskRepo.CreateTask(ctx, task)
 	if err != nil {
+		log.Error("failed to create task in repo", logger.Error(err))
+
 		return 0, err
 	}
+
+	log.Info("created task in repo", logger.Int("task id", id))
 
 	return id, nil
 }
 
 func (u *taskUsecase) GetAllTasks(ctx context.Context) ([]*model.Task, error) {
+	const fn = "taskUsecase.GetAllTasks"
+	log := u.log.With(logger.String("fn", fn))
+
 	tasks, err := u.taskRepo.GetAllTasks(ctx)
 	if err != nil {
+		log.Error("failed to get all tasks from repo", logger.Error(err))
+
 		return nil, err
 	}
 
@@ -49,12 +62,19 @@ func (u *taskUsecase) GetAllTasks(ctx context.Context) ([]*model.Task, error) {
 		return tasks[i].ID < tasks[j].ID
 	})
 
+	log.Info("got all tasks from repo", logger.Int("tasks count", len(tasks)))
+
 	return tasks, nil
 }
 
 func (u *taskUsecase) GetTaskByID(ctx context.Context, id int) (*model.Task, error) {
+	const fn = "taskUsecase.GetTaskByID"
+	log := u.log.With(logger.String("fn", fn))
+
 	exist, err := u.taskRepo.IsTaskExistByID(ctx, id)
 	if err != nil {
+		log.Error("failed to check if task exist in repo", logger.Error(err))
+
 		return nil, err
 	}
 
@@ -64,15 +84,24 @@ func (u *taskUsecase) GetTaskByID(ctx context.Context, id int) (*model.Task, err
 
 	task, err := u.taskRepo.GetTaskByID(ctx, id)
 	if err != nil {
+		log.Error("failed to get task from repo", logger.Error(err))
+
 		return nil, err
 	}
+
+	log.Info("got task from repo", logger.Int("task id", id))
 
 	return task, nil
 }
 
 func (u *taskUsecase) UpdateTask(ctx context.Context, task *model.Task) error {
+	const fn = "taskUsecase.UpdateTask"
+	log := u.log.With(logger.String("fn", fn))
+
 	exist, err := u.taskRepo.IsTaskExistByID(ctx, task.ID)
 	if err != nil {
+		log.Error("failed to check if task exist in repo", logger.Error(err))
+
 		return err
 	}
 
@@ -86,15 +115,24 @@ func (u *taskUsecase) UpdateTask(ctx context.Context, task *model.Task) error {
 
 	err = u.taskRepo.UpdateTask(ctx, task)
 	if err != nil {
+		log.Error("failed to update task in repo", logger.Error(err))
+
 		return err
 	}
+
+	log.Info("updated task in repo", logger.Int("task id", task.ID))
 
 	return nil
 }
 
 func (u *taskUsecase) DeleteTask(ctx context.Context, id int) error {
+	const fn = "taskUsecase.DeleteTask"
+	log := u.log.With(logger.String("fn", fn))
+
 	exist, err := u.taskRepo.IsTaskExistByID(ctx, id)
 	if err != nil {
+		log.Error("failed to check if task exist in repo", logger.Error(err))
+
 		return err
 	}
 
@@ -104,8 +142,12 @@ func (u *taskUsecase) DeleteTask(ctx context.Context, id int) error {
 
 	err = u.taskRepo.DeleteTask(ctx, id)
 	if err != nil {
+		log.Error("failed to delete task in repo", logger.Error(err))
+
 		return err
 	}
+
+	log.Info("deleted task in repo", logger.Int("task id", id))
 
 	return nil
 }
